5f23efada00b4d7c2f279b948d6809cf786c9736
Alexrubio14 <alejandro.rubio@enti.cat>
2024-10-23 13:54:21 +0200

    Obstacle manager

---
+++ SDL_Steering_Behaviors/ObstacleManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Obstacle.h"
+
+#include <vector>
+
+#define OM ObstacleManager::Instance()
+
+class ObstacleManager
+{
+private:
+
+	std::vector<Obstacle*> obstacles;
+
+	ObstacleManager() = default;
+	~ObstacleManager();
+	ObstacleManager(const ObstacleManager&) = delete;
+	ObstacleManager& operator =(const ObstacleManager&) = delete;
+
+public:
+
+	inline static ObstacleManager& Instance()
+	{
+		static ObstacleManager manager;
+		return manager;
+	}
+
+	inline std::vector<Obstacle*> GetObstacles() { return obstacles; }
+	inline void SetObstacles(std::vector<Obstacle*> _obstacles) { obstacles = _obstacles; }
+};
+
+
+
